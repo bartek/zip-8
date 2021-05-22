56610e1a3a5b13cc72c76bdbe5762ae5e1e2de40
Bartek Ciszkowski <bartek@wattpad.com>
2021-05-22 10:17:28 -0300

    Starting to implement DXYN

--- cpu.zig
+++ cpu.zig
@@ -14,7 +14,11 @@ pub const CPU = struct {
 
     // Registers
     index_register: u16,
+
+    // Display based registers
     vx_register: u16,
+    vy_register: u16,
+    vf_register: u16,
 
     pub fn init(cpu: *CPU, mem: *memory.Memory, dis: *display.Display) void {
         cpu.memory = mem;
@@ -23,6 +27,8 @@ pub const CPU = struct {
         cpu.pc = 0x0200;
         cpu.index_register = 0x000;
         cpu.vx_register = 0x000;
+        cpu.vy_register = 0x000;
+        cpu.vf_register = 0x000;
     }
 
     pub fn deinit(cpu: *CPU, alloc: *std.mem.Allocator) void {
@@ -33,9 +39,6 @@ pub const CPU = struct {
         var opcode = cpu.fetch();
         var instruction = cpu.decode(opcode);
 
-        // opcode = cpu.fetch();
-        // cpu.execute(opcode)
-        // cpu.pc += 2
         return;
     }
 
@@ -76,12 +79,32 @@ pub const CPU = struct {
             },
             0xa000 => {
                 warn("Set index register to NNN", .{});
-                // Set index register to NNN
                 var v = opcode & 0x0FFF;
                 cpu.index_register = v;
             },
             0xd000 => {
+                // DXYN
+                // Draw an N pixels tall sprite from the memory location that
+                // the I index register is holding to the sreen, at the horizontal X
+                // coordinate in VX register and the Y coordinate in VY register
                 warn("Draw a sprite at (VX, Y) that is n rows tall.", .{});
+
+                // Get X and Y from appropriate registers
+                var x = cpu.vx_register;
+                var y = cpu.vy_register;
+
+                // Set VF to 0
+                cpu.vf_register = 0x000;
+
+                // For N rows
+                var rows = opcode & 0x000F;
+                var row: u8 = 0;
+                while (row < rows) : (row += 1) {
+                    warn("Drawing Row {d}", .{row});
+                    // Get one byte of sprite data from the memory address in
+                    // the I register
+                    var sprite_byte = cpu.memory.read(@intCast(u12, cpu.index_register));
+                }
             },
             0x6000 => {
                 warn("Set VX to NN", .{});
